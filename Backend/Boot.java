389d5ccebd5b23f589bc33f652b6f69ce9e6576a
sathviksimhap <sathviksimhap@gmail.com>
2025-06-19 18:23:30 +0530

    Creating Springs and Boots

---
+++ Backend/Boot.java
@@ -0,0 +1,9 @@
+//for creating and handling boots
+class Boot{
+    public boolean isSpring(){
+        return false;
+    }
+    public boolean isBoot(){
+        return true;
+    }
+}
---
+++ Backend/Spring.java
@@ -0,0 +1,9 @@
+//for creating and handling springs
+class Spring{
+    public boolean isSpring(){
+        return true;
+    }
+    public boolean isBoot(){
+        return false;
+    }
+}
